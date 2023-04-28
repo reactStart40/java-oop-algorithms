48906e0b3ae4697408b76e7a9ecb9c748fa16e5f
Olga Greyskop <o.greyskop@gmail.com>
2023-04-28 16:21:55 +0300

    CW#7 algorithm complexities

---
+++ src/telran/algorithm/initialAlgorithms.java
@@ -0,0 +1,5 @@
+package telran.algorithm;
+
+public class initialAlgorithms {
+
+}
--- src/telran/util/Collection.java
+++
@@ -1,9 +0,0 @@
-package telran.util;
-
-public interface Collection <T>{
-boolean add(T obj);
- int size();
- boolean remove (T pattern);
- T[] toArray(T[]array);
- 
-}
