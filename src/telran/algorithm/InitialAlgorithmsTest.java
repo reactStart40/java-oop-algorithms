2ea4a353ad07a8dac72321c40dacfdf462eafcf0
Olga Greyskop <o.greyskop@gmail.com>
2023-04-30 23:38:21 +0300

    HW#7 version Yura

---
+++ src/telran/algorithm/InitialAlgorithmsTest.java
@@ -0,0 +1,68 @@
+package telran.algorithm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static telran.algorithm.initialAlgorithms.*;
+
+class InitialAlgorithmsTest {
+ int N_NUMBERS = 100000;
+ short[] array;
+
+ void setUpBigArray() {
+	 
+	 array = new short[N_NUMBERS];
+	 for(int i = 0; i < N_NUMBERS; i++) {
+		 array[i] = (short) (Math.random() * Short.MAX_VALUE);
+	 }
+ }
+	@Test
+	@Disabled
+	void bubbleSortTest() {
+		setUpBigArray();
+		bubbleSort(array);
+		runTest();
+	}
+	@Test
+	void SortPositiveShortTest() {
+		setUpBigArray();
+		sortShortPositive(array);
+		runTest();
+	}
+	private void runTest() {
+		for(int i = 1; i < N_NUMBERS; i++) {
+			assertTrue(array[i - 1] <= array[i]);
+		}
+		
+	}
+	@Test
+	void isSum2Test() {
+		short[] array = {30000, 1, 5, 2, 10000, 0, 500,0};
+		short[] array1 = {30000, 1, 5, 2, 10000, 0, 500,0, Short.MAX_VALUE};
+		assertTrue(isSum2(array, (short)30000));
+		assertTrue(isSum2(array, (short)7));
+		assertFalse(isSum2(array, (short)30003));
+		assertFalse(isSum2(array, (short)8));
+		assertTrue(isSum2(array1, Short.MIN_VALUE));
+		
+	}
+	@Test
+	void getMaxPositiveWithNegativeTest() {
+		short[] array = {1, 1, 1, -1, 20, 100,200, 100 -100, -100, -20, -40, 80};
+		short[] array1 = {-40, 1, -40, -6, 2, 3, 40};
+		short[] array2 = {40, 1, 2, 3, 40, -30};
+		assertEquals(100,
+				getMaxPositiveWithNegativeReflect(array));
+		assertEquals(40,
+				getMaxPositiveWithNegativeReflect(array1));
+		assertEquals(-1,
+				getMaxPositiveWithNegativeReflect(array2));
+	}
+
+}
--- src/telran/algorithm/initialAlgorithms.java
+++ src/telran/algorithm/initialAlgorithms.java
@@ -1,71 +1,90 @@
 package telran.algorithm;
-import telran.algorithm.*;
-import java.util.Set;
-import java.util.Arrays;
-import java.util.HashSet;
 
-
-@SuppressWarnings("unused")
 public class initialAlgorithms {
-public static void sortShortPositive(short []array) {
-	int []helper = new int [Short.MAX_VALUE];
-	for(int i=0; i<array.length; i++) {
-		helper[array[i]]++;
-	}
-	 int ind=0;
-	for (int i=0; i<helper.length; i++) {
-		for (int j=0; j<helper [i]; j++) {
-			array[ind++]=(short) i;
+	public static void sortShortPositive(short[] array) {
+		int[] helper = new int[Short.MAX_VALUE];
+		// helper[index] => count of occurrences for number index in array
+		// helper[1000] = 2 => number 1000 occurs twice in the source array
+		// helper[2] = 0;
+		for (int i = 0; i < array.length; i++) {
+			helper[array[i]]++;
 		}
+		int ind = 0;
+		for (int i = 0; i < helper.length; i++) {
+			for (int j = 0; j < helper[i]; j++) {
+				array[ind++] = (short) i;
+			}
+		}
+
+	}
+
+	public static boolean isSum2(short[] array, short sum) {
+		// returns true if there are two numbers in the given array,
+		// sum of which equals the given sum value
+		// otherwise false
+	
+		int helperSize = sum < 0 ? Short.MAX_VALUE + 1 : sum + 1;
+		boolean[] helper = new boolean[helperSize];
+		boolean res = false;
+		int index = 0;
+		while (index < array.length && !res) {
+			short value = array[index];
+			short secondValue = (short) (sum - value);
+			if (secondValue >= 0) {
+				if (helper[secondValue]) {
+					res = true;
+				} else {
+					helper[value] = true;
+				}
+			}
+			index++;
+		}
+		return res;
+	}
+
+	public static short getMaxPositiveWithNegativeReflect(short[] array) {
+		// returns maximal positive number, for which there is the negative image
+		// If there are not such numbers at all the method returns -1
+		short res = -1;
+		byte[] helper = new byte[Short.MAX_VALUE];
+		short candidate = -1;
+		for (int i = 0; i < array.length; i++) {
+			candidate = (short) Math.abs(array[i]);
+			if (array[i] < 0) {
+
+				if (helper[candidate] == 1 && candidate > res) {
+					res = candidate;
+				} else if (helper[candidate] == 0) {
+					helper[candidate] = -1;
+				}
+			} else {
+				if (helper[candidate] == -1 && candidate > res) {
+					res = candidate;
+				} else if (helper[candidate] == 0) {
+					helper[candidate] = 1;
+				}
+			}
+		}
+
+		return res;
+	}
+
+	public static void bubbleSort(short[] array) {
+		// sorting with bubble sort algorithm
+		int size = array.length;
+		boolean flUnsorted = false;
+		do {
+			size--;
+			flUnsorted = false;
+			for (int i = 0; i < size; i++) {
+				if (array[i] > array[i + 1]) {
+					short tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					flUnsorted = true;
+				}
+			}
+		} while (flUnsorted);
+
 	}
 }
-
-public static boolean isSum2(short[] array, short sum) {
-    Set<Short> set = new HashSet<>();
-    for (short element : array) {
-        if (set.contains((short)(sum - element))) {
-            return true;
-        }
-        set.add(element);
-    }
-    return false;
-}
-
-public static void bubbleSort(short[] arr) {
-    if (arr == null || arr.length == 0) {
-        return;
-    }
-
-    boolean sorted = false;
-    int lastSwapIndex = arr.length - 1;
-    while (!sorted) {
-        sorted = true;
-        int tempIndex = lastSwapIndex;
-        for (int i = 0; i < tempIndex; i++) {
-            if (arr[i] > arr[i+1]) {
-                short temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-                sorted = false;
-                lastSwapIndex = i;
-            }
-        }
-    }
-}
-
-
-public static short getMaxPositiveWithNegativeReflect(short[] array) {
-    bubbleSort(array);
-    int maxPositive = -1;
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] > 0 && Arrays.binarySearch(array, (short) -array[i]) >= 0) {
-            if (maxPositive == -1 || array[i] > maxPositive) {
-                maxPositive = array[i];
-            }
-        }
-    }
-    return (short) maxPositive;
-}
-
-}
-
