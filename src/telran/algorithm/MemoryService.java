8eb71626148ea2f79d71d0a619894b2efff79a0c
Olga Greyskop <o.greyskop@gmail.com>
2023-05-07 18:33:52 +0300

    HW#9 getMaxAvailableSize

--- src/telran/algorithm/MemoryService.java
+++ src/telran/algorithm/MemoryService.java
@@ -1,19 +1,22 @@
 package telran.algorithm;
 
 public class MemoryService {
-	
-		public static int getMaxAvailableSize() {
-			int res = Integer.MAX_VALUE;
-			boolean running = true;
-			while (running) {
-				try {
-					byte[] array = new byte[res];
-					running = false;
-				} catch(OutOfMemoryError e) {
-					res /= 2;
-				}
-			}
-			return res;
-			
+public static int getMaxAvailableSize() {
+	int right = Integer.MAX_VALUE;
+	int left = 0;
+	int middle = right / 2;
+	int maxSize = 0;
+	while (left <= right) {
+		try {
+			byte[] array = new byte[middle];
+			maxSize = middle;
+			left = middle + 1;
+		} catch(OutOfMemoryError e) {
+			right = middle - 1;
 		}
+		middle = right / 2 + left / 2;
 	}
+	return maxSize;
+	
+}
+}
