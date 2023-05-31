6bf1f5c71b9ba64f25c5a153371831c9e8b39819
Olga Greyskop <o.greyskop@gmail.com>
2023-05-31 18:36:33 +0300

    CW#16 TreeSet

---
+++ src/telran/util/TreeSet.java
@@ -0,0 +1,37 @@
+package telran.util;
+
+import java.util.Iterator;
+
+public class TreeSet<T> implements Set<T> {
+
+	@Override
+	public boolean add(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean remove(T pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(T pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
